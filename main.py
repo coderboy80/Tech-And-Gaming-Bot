884cdaf5a730461b564d1919dfdd26834c6c243f
Simply_Code <simplycode07@gmail.com>
2021-02-04 08:51:39 +0530

    Initialise commit

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import discord
 import random
 from discord.ext import commands
-import os
+#import os
 
-token = os.environ.get("TECH_AND_GAMING_BOT")
+#token = os.environ.get("TECH_AND_GAMING_BOT")
+token = "ODA2NzE3MjU3MTkwOTk4MDI3.YBtf_g.p_Gf07j9hj8MOPAKnUP1u82ZtdE"
 
 client = commands.Bot(command_prefix = ",")
 
@@ -21,7 +22,8 @@ async def unban(ctx, *, member):
     banned_user = await ctx.guild.bans()
     member_name, member_discriminator = member.split("#")
 
-    for banned_entry in banned_users:
+    for banned_entry in banned_user:
+        user = banned_entry.user
         if (user.name, user.discriminator) == (member_name, member_discriminator):
             await ctx.guild.unban(user)
             await ctx.send(f"Unbanned {user.mention}")
